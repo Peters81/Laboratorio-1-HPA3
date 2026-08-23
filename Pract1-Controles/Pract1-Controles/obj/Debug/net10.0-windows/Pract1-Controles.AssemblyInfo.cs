bca7325f2a49bbbc84683a855166ed879ea29aae
Peters <peterscristell18@gmail.com>
2026-08-23 01:17:52 -0500

    cambio fast

--- Pract1-Controles/Pract1-Controles/obj/Debug/net10.0-windows/Pract1-Controles.AssemblyInfo.cs
+++ Pract1-Controles/Pract1-Controles/obj/Debug/net10.0-windows/Pract1-Controles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pract1-Controles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf289a1a14d3dc2905d876d16782638dd9764ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pract1-Controles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pract1-Controles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
