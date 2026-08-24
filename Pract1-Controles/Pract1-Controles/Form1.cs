1c920d53946125e1cccc7ceee50923fa2ee73d5d
Peters <peterscristell18@gmail.com>
2026-08-24 10:28:09 -0500

    x

--- Pract1-Controles/Pract1-Controles/Form1.cs
+++ Pract1-Controles/Pract1-Controles/Form1.cs
@@ -7,21 +7,25 @@ namespace Pract1_Controles
             InitializeComponent();
         }
 
-     
+
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
             if (txtDia.Text != "" && txtMes.Text != "" && txtAnio.Text != "")
             {
-                respuesta.Text = txtDia.Text + "/" + txtMes.Text + "/" + txtAnio.Text;
+                if (int.TryParse(txtDia.Text, out _) && int.TryParse(txtMes.Text, out _) && int.TryParse(txtAnio.Text, out _))
+                {
+                    respuesta.Text = txtDia.Text + "/" + txtMes.Text + "/" + txtAnio.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Los campos deben ser números válidos");
+                }
             }
-
-
             else
             {
                 MessageBox.Show("Debe ingresar todo los campos");
             }
-
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
